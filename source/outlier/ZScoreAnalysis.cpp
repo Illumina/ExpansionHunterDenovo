4df6590a538c2824ad01d1faa1dcb94e058bc67d
egor-dolzhenko <egor.dolzhenko@gmail.com>
2021-05-02 21:20:16 -0700

    Implement basic quantile bootstrapping

--- source/outlier/ZScoreAnalysis.cpp
+++ source/outlier/ZScoreAnalysis.cpp
@@ -22,4 +22,60 @@
 
 #include "outlier/ZScoreAnalysis.hh"
 
-ZScoreAnalysisResults analyzeZScores(const Manifest& manifest, const AnchoredIrrCounts& irrCounts) { return { 1, {} }; }
+#include <algorithm>
+#include <random>
+#include <vector>
+
+using std::vector;
+
+ZScoreAnalysisResults analyzeZScores(const Manifest& manifest, const AnchoredIrrCounts& irrCounts)
+{
+    vector<double> rawCounts;
+    rawCounts.reserve(manifest.size());
+
+    for (const auto& sampleAndStatus : manifest)
+    {
+        const auto& sample = sampleAndStatus.first;
+        auto record = irrCounts.countBySample.find(sample);
+        if (record != irrCounts.countBySample.end())
+        {
+            rawCounts.push_back(record->second);
+        }
+        else
+        {
+            rawCounts.push_back(0);
+        }
+    }
+
+    const int kNumResamples = 100;
+    vector<double> resample;
+    resample.reserve(rawCounts.size());
+    const int targetQuantileIndex = static_cast<int>(std::round((rawCounts.size() - 1) * 0.95));
+
+    std::random_device device;
+    std::mt19937 engine { device() };
+    std::uniform_int_distribution<int> randomIndex(0, rawCounts.size() - 1);
+
+    vector<double> quantiles;
+    quantiles.reserve(kNumResamples);
+    for (int resampleIndex = 0; resampleIndex != kNumResamples; ++resampleIndex)
+    {
+        resample.clear();
+        for (int countIndex = 0; countIndex != rawCounts.size(); ++countIndex)
+        {
+            resample.push_back(rawCounts[randomIndex(engine)]);
+        }
+
+        std::nth_element(resample.begin(), resample.begin() + targetQuantileIndex, resample.end());
+        quantiles.push_back(resample[targetQuantileIndex]);
+    }
+
+    // std::sort(resample.begin(), resample.end());
+    for (auto value : quantiles)
+    {
+        std::cerr << value << " ";
+    }
+    std::cerr << std::endl;
+
+    return { 1, {} };
+}
