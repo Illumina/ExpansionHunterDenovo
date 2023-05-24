38d3b9754d6a4f99f0766dc021572fa1c812dc0a
Guillaume Rizk <grizk@illumina.com>
2023-05-24 17:32:34 +0200

    update source files license header

--- source/io/HtsFileStreamer.cpp
+++ source/io/HtsFileStreamer.cpp
@@ -6,19 +6,18 @@
 // Author: Egor Dolzhenko <edolzhenko@illumina.com>,
 //         Michael Eberle <meberle@illumina.com>
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the PolyForm Strict License 1.0.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      https://polyformproject.org/licenses/strict/1.0.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// As far as the law allows, the software comes as is, without
+// any warranty or condition, and the licensor will not be liable
+// to you for any damages arising out of these terms or the use
+// or nature of the software, under any kind of legal claim.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-//
 
 #include "io/HtsFileStreamer.hh"
 
